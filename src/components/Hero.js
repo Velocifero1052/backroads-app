a4f04abf6760d04bb24c991cd28a46c9c45080f6
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-07 10:16:25 +0500

    title separate component and corresponding refactoring, images fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 function Hero(){
     return (<section className="hero" id="home">
         <div className="hero-banner">
-            <h1>continue exploring</h1>
+            <h1>backroads app</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                explicabo debitis est autem dicta.
+                explicabo deb itis est autem dicta.
             </p>
             <a href="#tours" className="btn hero-btn">explore tours</a>
         </div>
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+function Title(props){
+    return (
+        <div className="section-title">
+            <h2>
+                {props.title} <span>{props.subTitle}</span>
+            </h2>
+        </div>
+    );
+}
+
+export default Title;
