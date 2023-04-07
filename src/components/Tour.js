36694912bd48d59234b2c69e852a8b5a7537eff9
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-07 11:38:23 +0500

    tours decomposition

---
+++ src/components/Tour.js
@@ -0,0 +1,28 @@
+function Tour(props) {
+    return (
+        <article className="tour-card">
+            <div className="tour-img-container">
+                <img src={props.image} className="tour-img" alt=""/>
+                <p className="tour-date">{props.date}</p>
+            </div>
+            <div className='tour-info'>
+                <div className="tour-title">
+                    <h4>{props.name}</h4>
+                </div>
+
+                <p>
+                    {props.text}
+                </p>
+                <div className="tour-footer">
+                    <p>
+                        <span><i className="fas fa-map"></i></span> china
+                    </p>
+                    <p>{props.duration}</p>
+                    <p>from ${props.price}</p>
+                </div>
+            </div>
+        </article>
+    );
+}
+
+export default Tour;
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,5 +1,6 @@
 import Title from "./Title";
 import {tours} from "../data";
+import Tour from "./Tour";
 
 function Tours(){
     return (
@@ -8,28 +9,9 @@ function Tours(){
             <div className="section-center featured-center">
                 {tours.map((tour) => {
                     return (
-                        <article key={tour.id} className="tour-card">
-                            <div className="tour-img-container">
-                                <img src={tour.image} className="tour-img" alt=""/>
-                                <p className="tour-date">{tour.date}</p>
-                            </div>
-                            <div className='tour-info'>
-                                <div className="tour-title">
-                                    <h4>{tour.name}</h4>
-                                </div>
-
-                                <p>
-                                    {tour.text}
-                                </p>
-                                <div className="tour-footer">
-                                    <p>
-                                        <span><i className="fas fa-map"></i></span> china
-                                    </p>
-                                    <p>{tour.duration}</p>
-                                    <p>from ${tour.price}</p>
-                                </div>
-                            </div>
-                        </article>
+                        <Tour key={tour.id} image={tour.image} date={tour.date} name={tour.name} text={tour.text}
+                            duration={tour.duration} price={tour.price}
+                        />
                     )})
                 }
             </div>
